341c534234bf0a9d89e95d9b68eaf93360ebdaf7
Rafnix Guzman <rafnixg@gmail.com>
2020-02-19 21:14:05 -0500

    [IMP] Separa la data demo de la logica de la vista.

--- run-dev.sh
+++ run-dev.sh
@@ -3,7 +3,5 @@
 source env/bin/activate;
 echo ' * Virtual enviroment: Active';
 export FLASK_APP=run.py;
-export FLASK_DEBUG=True;
-echo ' * FLASK_APP: run.py';
-echo ' * FLASK_DEBUG: True';
+export FLASK_ENV=development;
 flask run --host=0.0.0.0 --port=5001;
